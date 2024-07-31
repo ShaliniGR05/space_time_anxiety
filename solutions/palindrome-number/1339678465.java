ec05f1e470d85489c98db9734bab3e027485f234
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2024-07-31 15:21:54 +0000

    LeetCode: Palindrome Number (1339678465)

---
+++ solutions/palindrome-number/1339678465.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev=0;
+        int rem=0;
+        if(x<0){
+            return false;
+        }
+        while(x!=0){
+            rem = x%10;
+            rev = rev*10 + rem;
+            x = x/10;
+        }
+        if(rev == temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
