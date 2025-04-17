3e82ade9dac866739c3cac0a479420a8d8fb979e
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-04-17 05:25:39 +0000

    LeetCode: Number of Steps to Reduce a Number to Zero (1609201765)

---
+++ solutions/number-of-steps-to-reduce-a-number-to-zero/1609201765.c
@@ -0,0 +1,14 @@
+int numberOfSteps(int num) {
+    int count = 0;
+    int n = num;
+    while(n!=0){
+        if(n%2==0) {
+            n = n/2;
+        }
+        else {
+            n = n-1;
+        }
+        count = count + 1;
+    }
+    return count;
+}
