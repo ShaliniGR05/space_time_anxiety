05f77a89a64d12e37194213695c86880f76036fa
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-04-20 03:54:28 +0000

    LeetCode: Type of Triangle (1612107257)

---
+++ solutions/type-of-triangle/1612107257.c
@@ -0,0 +1,6 @@
+char* triangleType(int* nums, int numsSize) {
+    if(nums[2]>=nums[0] + nums[1] || nums[0]>=nums[1]+nums[2] || nums[1]>=nums[0]+nums[2]) return "none";
+    if(nums[0]==nums[1] && nums[0]==nums[2] && nums[1]==nums[2]) return "equilateral";
+    else if(nums[0]!=nums[1] && nums[0]!=nums[2] && nums[1]!=nums[2]) return "scalene";
+    else return "isosceles";
+}
