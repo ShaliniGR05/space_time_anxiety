1fe7fe04a6ced36ea70c463bad97786e98764cbf
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-04-20 06:19:48 +0000

    LeetCode: Largest Odd Number in String (1612222460)

---
+++ solutions/largest-odd-number-in-string/1612222460.c
@@ -0,0 +1,10 @@
+char* largestOddNumber(char* num) {
+    int n = strlen(num);
+    for(int i = n;i>=0;i--){
+        if(num[i]% 2 == 1){
+            num[i+1] = '\0';
+            return num;
+        }
+    }
+    return "";
+}
