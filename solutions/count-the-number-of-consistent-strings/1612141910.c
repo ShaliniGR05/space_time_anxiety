105b6b56d9fc7da2b99eb3d1c1b2684cae221191
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-04-20 04:38:44 +0000

    LeetCode: Count the Number of Consistent Strings (1612141910)

---
+++ solutions/count-the-number-of-consistent-strings/1612141910.c
@@ -0,0 +1,20 @@
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+    int map[26] = {0};
+    for(int i=0;allowed[i];i++){
+        map[allowed[i] - 'a'] = 1;
+    }
+    int count = 0;
+    for(int i=0;i<wordsSize;i++){
+        int check = 1;
+        for(int j=0;words[i][j];j++){
+            if(!map[words[i][j] - 'a']){
+                check = 0;
+                break;
+            }
+        }
+        if(check){
+            count += 1;
+        }
+    }
+    return count;
+}
