c1685702a0d602676f9911dc48b1ebff479ca4cc
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-04-21 08:13:23 +0000

    LeetCode: Best Time to Buy and Sell Stock (1613307720)

---
+++ solutions/best-time-to-buy-and-sell-stock/1613307720.c
@@ -0,0 +1,14 @@
+int maxProfit(int* prices, int pricesSize) {
+    int max = 0;
+    int min = prices[0];
+    for(int i=1;i<pricesSize;i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }
+        else if(prices[i]-min > max){
+            max = prices[i] - min;
+        }
+    }
+    return max;
+    
+}
