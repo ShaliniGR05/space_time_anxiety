cf05cd41f7134259f0345776268b776802bf4faa
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2025-12-28 18:05:55 +0000

    LeetCode: Group Anagrams (1867800743)

---
+++ solutions/group-anagrams/1867800743.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> result = new HashMap<>();
+        for(String word: strs){
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!result.containsKey(key)){
+                result.put(key,new ArrayList<>());
+            }
+            result.get(key).add(word);
+        }
+        return new ArrayList<>(result.values());
+    }
+}
