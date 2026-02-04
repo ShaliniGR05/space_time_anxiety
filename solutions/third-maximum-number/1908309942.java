e118ee9dc7884410a44ae13c9e8bb5fb2ddfdbdb
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-02-04 18:18:30 +0000

    LeetCode: Third Maximum Number (1908309942)

---
+++ solutions/third-maximum-number/1908309942.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        int n = nums.length;
+        Long max = null, second = null, third = null;
+        for(int num:nums){
+            if((max!=null && num == max) ||
+            (second!=null && num == second) ||
+            (third !=null && num == third)){
+                continue;
+            }
+            if(max == null || num>max){
+                third = second;
+                second = max;
+                max = (long) num;
+            }
+            else if(second == null || num>second){
+                third = second;
+                second = (long) num;
+            }
+            else if(third ==null ||num> third){
+                third = (long) num;
+            }
+        }
+        return third==null?max.intValue() : third.intValue();
+    }
+}
