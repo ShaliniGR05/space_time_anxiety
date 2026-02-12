f8fbcc591f71e5ee9083c69b2edded718089c6f0
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-02-12 18:34:31 +0000

    LeetCode: Binary Search (1917309851)

---
+++ solutions/binary-search/1917309851.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(end>=start){
+            int mid = (start+end)/2;
+            if(target==nums[mid]) return mid;
+            else if(target>nums[mid]) start = mid + 1;
+            else if(target<nums[mid]) end = mid-1;
+        }
+        return -1;
+    }
+}
