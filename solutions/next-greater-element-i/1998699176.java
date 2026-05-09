0aa2bd41d2fd32b334fc698d27599c1fb755c08a
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-05-09 09:21:41 +0000

    LeetCode: Next Greater Element I (1998699176)

---
+++ solutions/next-greater-element-i/1998699176.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        for(int i = nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i] >= st.peek()){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                hash.put(nums2[i],-1);
+            }
+            else{
+                hash.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = hash.get(nums1[i]);
+        }
+        return ans;
+    }
+}
