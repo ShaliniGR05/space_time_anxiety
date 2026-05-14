7223b53098373af975daa8ae5089bc7884b84644
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-05-14 17:07:30 +0000

    LeetCode: Sort an Array (2003167029)

---
+++ solutions/sort-an-array/2003167029.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i -1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+        return arr;
+        }
+    }
