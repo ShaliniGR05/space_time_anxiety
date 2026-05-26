63609372a6cc518cd970156bf14b437b0c5e0a5a
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-05-26 06:34:35 +0000

    LeetCode: Find Peak Element (2013313757)

---
+++ solutions/find-peak-element/2013313757.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(right>left){
+            int mid = (left + right)/2;
+            if(nums[mid]>nums[mid+1]) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
