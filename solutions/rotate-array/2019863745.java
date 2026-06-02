ae608c21dda2c2456f58e7d052102dd316ce5737
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-06-02 06:44:16 +0000

    LeetCode: Rotate Array (2019863745)

---
+++ solutions/rotate-array/2019863745.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = k % nums.length;
+        int[] temp = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int new_index = (i + k) % nums.length;
+            temp[new_index] = nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i] = temp[i];
+        }
+    }
+}
