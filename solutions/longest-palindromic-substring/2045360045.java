8f6e1f8658eeeba1d1eec3861270add787d64dad
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-06-25 05:52:54 +0000

    LeetCode: Longest Palindromic Substring (2045360045)

---
+++ solutions/longest-palindromic-substring/2045360045.java
@@ -0,0 +1,23 @@
+class Solution {
+    int start = 0;
+    int end = 0;
+    public String longestPalindrome(String s) {
+        for(int i=0;i<s.length();i++){
+            expand(s,i,i);
+            expand(s,i,i+1);
+        }
+        return s.substring(start,end+1);
+    }
+    public void expand(String s,int left, int right){
+        while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        left++;
+        right--;
+        if(right-left > end - start){
+            start = left;
+            end = right;
+        }
+    }
+}
