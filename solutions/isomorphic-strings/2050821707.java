c5c4d796d2fcfa2b1db49e21d92ce8fa9f59330b
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-06-30 06:13:57 +0000

    LeetCode: Isomorphic Strings (2050821707)

---
+++ solutions/isomorphic-strings/2050821707.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        HashMap<Character,Character> st = new HashMap<>();
+        HashMap<Character,Character> ts = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char chars = s.charAt(i);
+            char chart = t.charAt(i);
+            if(!st.containsKey(chars)){
+                st.put(chars,chart);
+            }
+            else{
+                if(st.get(chars)!=chart){
+                    return false;
+                }
+            }
+            if(!ts.containsKey(chart)){
+                ts.put(chart,chars);
+            }
+            else {
+                if(ts.get(chart)!=chars){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
