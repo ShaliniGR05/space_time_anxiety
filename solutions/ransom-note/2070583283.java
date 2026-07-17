f468a751309b9f8abdcb03f4e4d3e655e0a576bc
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-07-17 04:16:04 +0000

    LeetCode: Ransom Note (2070583283)

---
+++ solutions/ransom-note/2070583283.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }
+        if(ransomNote.length() == 1 && magazine.length() == 1 && !ransomNote.equals(magazine)){
+            return false;
+        }
+        HashMap<Character, Integer> res1 = new HashMap<>();
+        for (char ch : magazine.toCharArray()) {
+            res1.put(ch, res1.getOrDefault(ch, 0) + 1);
+        }
+        HashMap<Character, Integer> res2 = new HashMap<>();
+        for (char ch : ransomNote.toCharArray()) {
+            res2.put(ch, res2.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : res2.keySet()) {
+            if(!(res1.getOrDefault(ch, 0)>=res2.get(ch))){
+                return false;
+            }
+        }
+        return true;
+        }
+    }
