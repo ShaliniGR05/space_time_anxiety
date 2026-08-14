46c9e10067ad55c72bd3f398563fe86b340df4b0
ShaliniGR05 <165385782+ShaliniGR05@users.noreply.github.com>
2026-08-14 03:58:45 +0000

    LeetCode: Find All Duplicates in an Array (2106115199)

---
+++ solutions/find-all-duplicates-in-an-array/2106115199.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> lst = new ArrayList<>();
+        for(int num:nums){
+            if(!set.contains(num)) set.add(num);
+            else lst.add(num);
+        }
+        return lst;
+    }
+}
